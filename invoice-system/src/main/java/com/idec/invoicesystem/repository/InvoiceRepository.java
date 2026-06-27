62b68c2142d82ff50bc957530717e6bac1433240
DinethmiPeiris <thinupeiris04@gmail.com>
2026-06-27 09:31:34 +0530

    Delete invoice-system directory

--- invoice-system/src/main/java/com/idec/invoicesystem/repository/InvoiceRepository.java
+++
@@ -1,6 +0,0 @@
-package com.idec.invoicesystem.repository;
-import com.idec.invoicesystem.model.Invoice;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InvoiceRepository extends MongoRepository<Invoice, String> {
-}
